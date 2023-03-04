05ad2b195c9399166dcde498acf8f0e31ea70760
Mario <mario_tanasa@yahoo.com>
2023-03-05 01:32:26 +0200

    small changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,109 @@
 import React from "react";
-import { Link } from "react-scroll";
-import { BiHomeAlt, BiUser } from "react-icons/bi";
-import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+import { useState } from "react";
+import { Transition } from "@headlessui/react";
+import { HomeIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav>
-      <div className="logo"></div>
-      <ul>
-        <li>
-          <a href="#">Acasa</a>
-        </li>
-        <li>
-          <a href="#">Despre Noi</a>
-        </li>
-        <li>
-          <a href="#">Servicii</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <img
+                className="h-8 w-8"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                alt="Logo"
+              />
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a
+                  href="#"
+                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Features
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Pricing
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Contact Us
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+              aria-label="Main menu"
+              aria-expanded={isOpen}
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {isOpen ? (
+                <XIcon className="h-6 w-6" aria-hidden="true" />
+              ) : (
+                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+      <Transition
+        show={isOpen}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        {(ref) => (
+          <div className="md:hidden" ref={ref}>
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <a
+                href="#"
+                className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Features
+              </a>
+              <a
+                href="#"
+                className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Pricing
+              </a>
+              <a
+                href="#"
+                className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Contact Us
+              </a>
+            </div>
+          </div>
+        )}
+      </Transition>
     </nav>
   );
 }
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 function Pricing() {
-  return <div>Pricing</div>;
+  // return (
+  //   <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  //     <div class="md:flex">
+  //       <div class="md:flex-shrink-0">
+  //         <img
+  //           class="h-48 w-full object-cover md:w-48"
+  //           src="https://via.placeholder.com/200x200"
+  //           alt="Service"
+  //         />
+  //       </div>
+  //       <div class="p-8">
+  //         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+  //           Service
+  //         </div>
+  //         <p class="mt-2 text-gray-500">
+  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  //           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+  //           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+  //           aliquip ex ea commodo consequat.
+  //         </p>
+  //         <div class="mt-4 flex items-center">
+  //           <div class="text-lg font-semibold">$100</div>
+  //           <div class="ml-4 text-gray-500">/hour</div>
+  //         </div>
+  //         <div class="mt-6">
+  //           <button class="rounded-full px-5 py-2 text-sm font-semibold text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+  //             Book Now
+  //           </button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default Pricing;
