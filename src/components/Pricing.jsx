7ff6254b7ab0b8596d3a00e0a317d2c31c33c40f
Mario <mario_tanasa@yahoo.com>
2023-07-31 15:58:19 +0300

    commit

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -28,7 +28,7 @@ function Pricing() {
                   />
                 </svg>
               </div>
-              <h3 className="service-title">POSTĂRI SOCIAL MEDIA</h3>
+              <h3 className="service-title">SOCIAL MEDIA CONTENT</h3>
               <p className="parag">
                 Echipa noastră specializată vă oferă servicii de creație de
                 postări pe social media, ce vor captiva audiența și vor defini
@@ -61,6 +61,32 @@ function Pricing() {
                 funcțional, oferindu-vă o prezență online de impact. Lăsați-ne
                 să construim website-ul perfect pentru succesul dvs. pe
                 internet!
+              </p>
+            </div>
+            <div className="card-container text-center bg-white">
+              <div className="svg-wraper">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-[30px] h-[30px]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42"
+                  />
+                </svg>
+              </div>
+              <h3 className="service-title">UI / UX</h3>
+              <p className="parag">
+                Serviciul nostru se concentrează pe proiectarea interfețelor
+                (UI) și a experiențelor de utilizare (UX) care pun utilizatorii
+                în centrul atenției. Procesul nostru creativ îmbină estetica
+                vizuală cu funcționalitatea, pentru a crea interfețe plăcute,
+                ușor de utilizat și eficiente.
               </p>
             </div>
           </div>
